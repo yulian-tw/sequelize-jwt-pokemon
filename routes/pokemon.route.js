74b6b6e3b9cb45b0f5371f330f72cbcaa6c68dc6
Yu Lian Ng <yulian.ng@thoughtworks.com>
2021-08-27 12:28:50 +0800

    add routes for get and post

--- routes/pokemon.route.js
+++ routes/pokemon.route.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("Hello World!");
+const db = require('../db/models/index');
+
+// GET
+router.get("/", async (req, res, next) => {
+    try {
+        const pokemons = await db.Pokemon.findAll();
+        res.json(pokemons);
+    } catch (error) {
+        next(error);
+    }
 });
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const pokemons = await db.Pokemon.findByPk(req.params.id);
+        res.json(pokemons);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// POST
+router.post("/", async (req, res, next) => {
+    try {
+        const pokemon = req.body;
+
+        const newPokemon = await db.Pokemon.create(pokemon);
+        res.status(201).json(newPokemon);
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 module.exports = router;
