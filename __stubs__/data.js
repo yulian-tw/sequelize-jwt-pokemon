7a9283f90443ed3339f42a7cf659a42186dadd96
Yu Lian Ng <yulian.ng@thoughtworks.com>
2021-08-27 12:46:14 +0800

    added pokemon stub

---
+++ __stubs__/data.js
@@ -0,0 +1,30 @@
+const pokemons = [
+    {
+        name: 'Pikachu',
+        japaneseName: 'ピカチュウ',
+        baseHP: 35,
+        category: 'Mouse Pokemon'
+    },
+    {
+        name: 'Squirtle',
+        japaneseName: 'ゼニガメ',
+        baseHP: 44,
+        category: 'Tiny Turtle Pokemon'
+    },
+    {
+        name: 'Wartortle',
+        japaneseName: 'カメール',
+        baseHP: 59,
+        category: 'Turtle Pokémon'
+    },
+    {
+        name: 'Meowth',
+        japaneseName: 'ニャース',
+        baseHP: 40,
+        category: 'Scratch Cat Pokémon'
+    }
+];
+
+module.exports = {
+    pokemons
+};
